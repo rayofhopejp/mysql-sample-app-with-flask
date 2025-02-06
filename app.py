5a2b7b2343b6d1b4da311c32b2e276aa30ed0d83
Ray <ray.oshikawa@gmail.com>
2025-02-06 20:19:23 +0900

    Update app.py: port 5000

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def delete_resident(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
